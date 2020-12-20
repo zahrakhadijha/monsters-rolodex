acedda1e2300dc51cce8929b895026712205049f
zahrakhadijha <zahra.khan1@icloud.com>
2020-12-20 17:51:50 -0500

    update favicon

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,30 +1,30 @@
 import React, { Component } from 'react'
-import './App.css';
+import './App.css'
 
 class App extends Component {
-    constructor() {
-        super()
+	constructor() {
+		super()
 
-        this.state = {
-            monsters: []
-        }
-    }
+		this.state = {
+			monsters: [],
+		}
+	}
 
-    componentDidMount() {
-        fetch('https://jsonplaceholder.typicode.com/users').then(response => response.json())
-        .then(users => this.setState({ monsters: users}))
-    }
+	componentDidMount() {
+		fetch('https://jsonplaceholder.typicode.com/users')
+			.then((response) => response.json())
+			.then((users) => this.setState({ monsters: users }))
+	}
 
-
-    render() {
-        return (
-					<div className='App'>
-                {this.state.monsters.map(monster => (
+	render() {
+		return (
+			<>
+				{this.state.monsters.map((monster) => (
                     <h1 key={monster.id}> {monster.name} </h1>
-                        ))}
-					</div>
-				)
-    }
+				))}
+			</>
+		)
+	}
 }
 
-export default App;
+export default App
